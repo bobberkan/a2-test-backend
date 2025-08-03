6bc106dae030b3fee38931b762ff1ff13c44f73b
Bobomurod Xudayberganov <khudayberganovbobomurod@gmail.com>
2025-08-03 23:57:31 +0500

    edit routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const express = require('express')
+const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/authController')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 
-module.exports = router;
+module.exports = router
