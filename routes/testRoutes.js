fe32d0a65b5c50f3bffda68d822262845817f48d
Bobomurod Xudayberganov <khudayberganovbobomurod@gmail.com>
2025-08-05 14:31:37 +0500

    edited

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const router = express.Router()
-const { protect, teacherOnly } = require('../middlewares/authMiddleware')
+const express = require('express');
+const router = express.Router();
+const { protect, teacherOnly } = require('../middlewares/authMiddleware');
 const {
   createTest,
   getAllTests,
   updateTest,
   deleteTest
-} = require('../controllers/testController')
+} = require('../controllers/testController');
 
-// Routes for Test CRUD (Teachers)
-router.post('/', protect, teacherOnly, createTest)
-router.get('/', protect, teacherOnly, getAllTests)
-router.put('/:id', protect, teacherOnly, updateTest)
-router.delete('/:id', protect, teacherOnly, deleteTest)
+// Student & Teacher ko'rishi mumkin:
+router.get('/', protect, getAllTests);
 
-module.exports = router
+// Teacher Only (create/update/delete)
+router.post('/', protect, teacherOnly, createTest);
+router.put('/:id', protect, teacherOnly, updateTest);
+router.delete('/:id', protect, teacherOnly, deleteTest);
+
+module.exports = router;
